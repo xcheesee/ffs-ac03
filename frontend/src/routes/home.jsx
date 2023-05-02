6fdd0774139807936f206ab3977a010399ef3d6e
João Lucas Silva Laurindo Cruz <x529427@rede.sp>
2023-05-02 10:08:30 -0300

    pog

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -1,21 +1,35 @@
 import { useEffect, useState } from "react"
 
 const Home = () => {
-	const [hello, setHello] = useState()
+	const [hello, setHello] = useState("")
 
 	useEffect(() => {
 		(async () => {
-			const res = await fetch("http://localhost:3030/")
+			const res = await fetch("http://localhost:5000/", {
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
 			if (res.ok) {
+				await new Promise((res, rej) => {
+					setTimeout(() => {
+						res()
+					}, 2000)
+				})
 				const json = await res.json()
 				return setHello(json)
 			}
 			throw {status: res.status, message: "Erro servidor"}
-		})
-	})
+		})();
+	}, [])
+	if (hello === "") {
+		return (
+			<div style={{fontSize: '64px', display: 'flex', justifyContent: 'center'}}>Carregando...</div>
+		)
+	}
 	return(
 		<>
-			<div style={{fontSize: '64px', display: 'flex', justifyContent: 'center'}}>Pagina Principal</div>
+			<div style={{fontSize: '64px', display: 'flex', justifyContent: 'center'}}>{hello.message}</div>
 		</>
 	)
 }
--- server/server.py
+++ server/server.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, render_template # Importa a biblioteca
+from flask import Flask, jsonify # Importa a biblioteca
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__) # Inicializa a aplicação
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/') # Cria uma rota
+@cross_origin()
 def main():
-  return "Hello Server"
+  return jsonify({"message": "Hello Server"})
 
 if __name__ == '__main__':
   app.run() # Executa a aplicação
