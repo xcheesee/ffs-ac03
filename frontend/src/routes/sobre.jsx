10d1dfdd9662a1420dd585230458326094893a15
João Lucas Silva Laurindo Cruz <x529427@rede.sp>
2023-05-05 09:57:26 -0300

    implementacao sem utilizar a lib flask-cors

--- frontend/src/routes/sobre.jsx
+++ frontend/src/routes/sobre.jsx
@@ -1,10 +1,36 @@
-const Sobre = () => {
-	return (
-		<>
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+export default function Sobre(){
+
+    const [api, setApi] = useState()
+
+    const getApi = async () => {
+        const response = await fetch(
+            "http://localhost:5000/"
+          ).then((response) => response.json());
+          setApi(response);
+    };
+
+      useEffect(() => {
+        getApi();
+      }, []);
+
+
+
+    return(
+        <div>
 			<div style={{fontSize: '64px', textTransform: 'capitalize', display: 'flex', justifyContent: 'center'}}>sobre nos</div>
 			<div style={{display: 'flex', justifyContent: 'center', paddingTop: '3rem'}}>Pagina de infos da empresa</div>
-		</>
-	)
+            <p>Server</p>
+            <div>
+                {api && true ?
+                <div>
+                    {api.message}
+                </div>
+				: <div></div>
+                }
+            </div>
+        </div>
+    );
 }
-
-export default Sobre
--- server/server.py
+++ server/server.py
@@ -1,14 +1,19 @@
-from flask import Flask, jsonify # Importa a biblioteca
-from flask_cors import CORS, cross_origin
+from flask import Flask, jsonify, request, make_response # Importa a biblioteca
 
 app = Flask(__name__) # Inicializa a aplicação
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+#app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/') # Cria uma rota
-@cross_origin()
+@app.route('/', methods=["GET", "OPTIONS"]) # Cria uma rota
 def main():
-  return jsonify({"message": "Hello Server"})
+  if request.method == "OPTIONS":
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    response.headers.add("Access-Control-Allow-Methods", "*")
+    return response
+  res = jsonify({"message": "Hello Server"})
+  res.headers.add("Access-Control-Allow-Origin", "*")
+  return res
 
 if __name__ == '__main__':
   app.run() # Executa a aplicação
